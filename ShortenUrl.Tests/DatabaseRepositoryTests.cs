a4bef4f6492f90632c365eeec871c041e94da136
x0rld <git@x0rld.eu>
2023-06-15 00:26:31 +0200

    add database insert with dapper

--- ShortenUrl.Tests/DatabaseRepositoryTests.cs
+++ ShortenUrl.Tests/DatabaseRepositoryTests.cs
@@ -4,34 +4,52 @@ using Microsoft.Data.Sqlite;
 
 namespace ShortenUrl.Tests;
 
-public class DatabaseRepositoryTests
+public class DatabaseRepositoryTests : IDisposable
 {
-    private SqliteConnection _connection;
-    private DbTransaction transaction;
+    private readonly DbTransaction _transaction;
+    private readonly IDatabaseRepository _sut;
 
     public DatabaseRepositoryTests()
     {
-        _connection = new SqliteConnection("Data Source=shortURLTest.sqlite");
-        _connection.Open();
+        var connection = new SqliteConnection("Data Source=shortURLTest.sqlite");
+        connection.Open();
         SQLitePCL.raw.SetProvider(new SQLitePCL.SQLite3Provider_e_sqlite3());
-        transaction = _connection.BeginTransactionAsync().GetAwaiter().GetResult();
-        _connection.QueryAsync<string>(
+        _transaction = connection.BeginTransaction();
+        _sut = new DatabaseRepository(connection);
+        connection.QueryAsync<string>(
             """
             INSERT INTO storedUrl (id, website) VALUES ('1234', 'https://twitch.tv/merry');
     """);
     }
-
-    ~DatabaseRepositoryTests()
-    {
-        transaction?.Dispose();
-    }
+    
     [Fact]
     public async Task QueryAsync_Should_Return_The_Line()
     {
         var expected= new StoredUrl("1234", "https://twitch.tv/merry");
-        var dynamicParameters = new DynamicParameters();
-        dynamicParameters.Add("@Id","1234");
-        var actual = await _connection.QueryFirstOrDefaultAsync<StoredUrl>("select * from storedUrl where id = @Id",dynamicParameters);
+        var actual = await _sut.QueryAsync<StoredUrl>("1234");
         Check.That(actual).IsEqualTo(expected);
+    }
+      
+    [Fact]
+    public async Task InsertAsync_Should_Not_Throw_An_Exception_When_Id_Doesnt_Exist()
+    {
+        Check.ThatCode(async () => await _sut.InsertAsync(new StoredUrl("4563", "https://twitch.tv/merry")))
+            .DoesNotThrow();
+    }
+
+
+    private void Dispose(bool disposing)
+    {
+        _transaction.Rollback();
+        if (disposing)
+        {
+            _transaction.Dispose();
+        }
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
     }
 }
--- ShortenUrl/DatabaseRepository.cs
+++ ShortenUrl/DatabaseRepository.cs
@@ -10,24 +10,38 @@ public class DatabaseRepository : IDatabaseRepository
     public DatabaseRepository(IDbConnection dbConnection)
     {
         _dbConnection = dbConnection;
-        
+        _dbConnection.Open();
     }
 
     public async Task<T> QueryAsync<T>(string id)
     {
-        _dbConnection.Open();
         var parameters = new DynamicParameters();
-        parameters.Add("@Id",id);
-        var result = await _dbConnection.QueryFirstOrDefaultAsync<T>("select * from storedUrl where id=@Id",parameters);
+        parameters.Add("@Id", id);
+        var result =
+            await _dbConnection.QueryFirstOrDefaultAsync<T>("select * from storedUrl where id=@Id", parameters);
         return result;
+    }
+
+    public async Task InsertAsync(StoredUrl info)
+    {
+        var parameters = new DynamicParameters();
+        parameters.Add("@Id", info.Id);
+        parameters.Add("@Url", info.Website);
+            await _dbConnection.QueryAsync(
+            "INSERT INTO storedUrl (id, website) VALUES (@Id, @Url);", parameters);
     }
 }
 
-
 // ReSharper disable once ClassNeverInstantiated.Global
-public record StoredUrl(string Id, string Website);
+public record StoredUrl(string Id, string Website)
+{
+    // ReSharper disable once UnusedMember.Local
+    private StoredUrl()  : this(default!, default!) { }
+};
 
 public interface IDatabaseRepository
 {
     Task<T> QueryAsync<T>(string id);
+
+    Task InsertAsync(StoredUrl info);
 }
